1b0cf8b66607f7b13982083ced1ebfb1cda82d32
craiglewis7 <craig@dev.ops.fish>
2017-06-24 16:43:54 +0100

    Created a new Delete AMIs script to be used with AWSCLI

---
+++ Delete-AMIs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import boto
+import sys
+
+REGION='eu-west-1'
+ACCESS=''
+SECRET=''
+
+def ec2delete(imageid=None):
+    conn = boto.ec2.connect_to_region('$REGION', aws_access_key_id='$ACCESS', aws_secret_access_key='$SECRET') 
+    conn.deregister_image(imageid, delete_snapshot=True)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+
+    options, args = parser.parse_args()
+    sys.stderr.write("Deleting %s and snapshots\n" %  str(args))
+    ec2delete(args)
